20f9291b18f1d45d08dfbf18d3268628af63bb52
Daniel Ilievski <dilievski54@gmail.com>
2026-02-10 21:30:11 +0100

    Add Cloudinary dependency and config

--- iskacame-backend/src/main/kotlin/mk/ukim/finki/iskacamebackend/config/ApplicationConfig.kt
+++ iskacame-backend/src/main/kotlin/mk/ukim/finki/iskacamebackend/config/ApplicationConfig.kt
@@ -1,8 +1,26 @@
 package mk.ukim.finki.iskacamebackend.config
 
+import com.cloudinary.Cloudinary
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class ApplicationConfig() {
+class ApplicationConfig {
 
+  @Bean
+  fun cloudinary(
+    @Value("\${cloudinary.cloud_name}") cloudName: String,
+    @Value("\${cloudinary.api_key}") apiKey: String,
+    @Value("\${cloudinary.api_secret}") apiSecret: String
+  ): Cloudinary {
+
+    val config = mapOf(
+      "cloud_name" to cloudName,
+      "api_key" to apiKey,
+      "api_secret" to apiSecret
+    )
+
+    return Cloudinary(config)
+  }
 }
