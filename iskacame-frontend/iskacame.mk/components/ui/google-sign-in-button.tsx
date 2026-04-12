c97a3e6796ce7836512b4b085bd085873d07820f
aesbube <ljubicad11@gmail.com>
2026-04-12 23:17:46 +0200

    remove ios client id

--- iskacame-frontend/iskacame.mk/components/ui/google-sign-in-button.tsx
+++ iskacame-frontend/iskacame.mk/components/ui/google-sign-in-button.tsx
@@ -14,9 +14,9 @@ export function GoogleSignInButton() {
     const { signInWithGoogle } = useAuth();
 
     const [request, response, promptAsync] = Google.useAuthRequest({
-        iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
-        androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
-        webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+        iosClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
+        androidClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
+        webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
         redirectUri,
     });
 
